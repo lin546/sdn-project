cea58a4cfc8b8372f9dc83d6124028bf033b6229
layne <ntes546@163.com>
2024-09-17 11:33:13 +0800

    chore：更新数据库文件

--- simulate/docs/project.sql
+++ simulate/docs/project.sql
@@ -1,4 +1,6 @@
 -- auto-generated definition
+
+# 用户表
 create table user
 (
     id    int auto_increment primary key,
@@ -8,3 +10,12 @@ create table user
 );
 
 INSERT INTO mydata.user (id, uname, pwd, root) VALUES (1, 'root', '21232f297a57a5a743894a0e4a801fc3', 1);
+
+# 流表
+CREATE TABLE `rules` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `hash` varchar(255) DEFAULT NULL,
+  `rule` varchar(255) DEFAULT NULL,
+  `uname` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
